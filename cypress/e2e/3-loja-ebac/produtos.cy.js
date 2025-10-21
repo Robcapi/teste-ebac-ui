21147807cd26990f697ce45eeec6cbf55b6dcebb
Robson Oliveira <robsoncapi7@gmail.com>
2025-10-21 11:14:48 -0300

    Listas

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block ')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Aero Daily Fitness Tee')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        
+
+        
+    });
+});
